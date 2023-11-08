b09954762da00adf9a7a29885c35a02fba08fa18
Belem Zhang <belem.zhang@intel.com>
2023-11-08 13:24:55 +0800

    11082023 directml

--- 1.16/common/dist/esm/training-session.js
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-import { TrainingSession as TrainingSessionImpl } from './training-session-impl.js';
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export const TrainingSession = TrainingSessionImpl;
-//# sourceMappingURL=training-session.js.map
